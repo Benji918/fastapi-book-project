a0c330642a4eac9be102058ed3eff32edd724f3c
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-02-12 19:47:37 +0100

    added test endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -70,4 +70,8 @@ async def get_book_by_id(book_id:int) -> Book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
+@router.get("/test-endpoint", response_model=Book, status_code=status.HTTP_200_OK)
+async def test_endpoint():
+    return JSONResponse(content='This is for testing sha')
+
 
